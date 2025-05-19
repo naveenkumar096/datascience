bc14b906a48850c4e49ca959882275736bee71b9
naveenkumar096 <naveenkumar096@users.noreply.github.com>
2025-05-19 10:19:58 +0530

    Add files via upload
    Model test by test data

--- logistic_regression.py
+++ logistic_regression.py
@@ -55,6 +55,7 @@ sns.set_style(style='darkgrid')
 sns.pairplot(split_cabin_long)
 #%%
 data = split_cabin_long
+data
 #%% md
 # ### Data Preprocessing:
 #%%
@@ -68,24 +69,21 @@ data3 = data[['Embarked']]
 numeric_data3 = encoder.fit_transform(data3)
 
 #%%
-data['cabin_numeric']=numeric_data
-data['Gender'] = numeric_data2
-data['Embarked_numeric'] = numeric_data3
+data['cabin']=numeric_data
+data['Sex'] = numeric_data2
+data['Embarked'] = numeric_data3
 
 #%%
-train_data = data.copy()   
+train_data = data.copy() 
+train_data
 #%%
-train_data.drop('Sex', axis=1, inplace=True)
-train_data.drop('Embarked', axis=1, inplace=True)
 train_data.drop('cabin_split', axis=1, inplace=True)
-train_data.drop('Fare', axis=1, inplace=True)
 #%%
-train_data.drop('Cabin', axis=1, inplace=True)
 
 #%%
 train_data.isnull().sum()
 train_data = train_data.dropna()
-
+train_data
 #%% md
 # ####  Model Building:
 #%%
@@ -95,7 +93,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 #%%
-X = train_data.iloc[:,1:]
+X = train_data[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
 Y = train_data.iloc[:,0]
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 #%%
@@ -146,6 +144,29 @@ plt.title('Receiver Operating Characteristic (ROC) Curve')
 plt.legend(loc='lower right')
 plt.show()
 #%%
+test_data = pd.read_csv('D:\\Data-science\\Titanic_test.csv')  # Replace with actual path or DataFrame
+
+# Preprocess test data
+test_data['Sex'] = test_data['Sex'].map({'male': 0, 'female': 1})
+test_data['Embarked'] = test_data['Embarked'].map({'C': 0, 'Q': 1, 'S': 2})
+test_data['Age'] = test_data['Age'].fillna(train_data['Age'].median())
+test_data['Fare'] = test_data['Fare'].fillna(train_data['Fare'].median())
+test_data['Embarked'] = test_data['Embarked'].fillna(train_data['Embarked'].mode()[0])
+
+test_data
+#%%
+X_test = test_data[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
+X_test
+#%%
+predictions = Classifier.predict(X_test)
+predictions
+#%%
+test_data['PredictedSurvived'] = predictions
+
+# View first few predictions
+test_data[['PassengerId', 'Name', 'PredictedSurvived']]
+
+#%%
 import streamlit as st
 
 #%%
@@ -156,20 +177,13 @@ st.write("""
 This app uses logistic regression to classify whether Titanic Passenger Survived ratio.
 """)
 #%%
-print(data['Embarked_numeric'].unique())
-#%%
-
-df = pd.DataFrame({'Cabin_name':data.cabin_split.unique(),'Cabin_No':data.cabin_numeric.unique()})
-df1=df[df['Cabin_name']=='C85']
-#%%
 pclass_input = st.sidebar.selectbox('Pclass', train_data.Pclass.unique())
 age_input = st.sidebar.number_input('Please enter age', min_value=0,max_value=150)
+fare_input = st.sidebar.number_input('Please enter Fare', min_value=0,max_value=10000)
 sib_input =st.sidebar.selectbox('SibSp', train_data.SibSp.unique())
 prach_input =st.sidebar.selectbox('prach', train_data.Parch.unique())
-cabin_input =st.sidebar.selectbox('Cabin', data.cabin_split.unique())
 Gender_input =st.sidebar.selectbox('Gender', data.Sex.unique())
 Embarked_numeric_input =st.sidebar.selectbox('Embraked', data.Embarked.unique())
-df1=df[df['Cabin_name']==cabin_input]
 gender = 0
 if Gender_input == 'Male': gender = 1
 
@@ -183,7 +197,7 @@ elif Embarked_numeric_input == 'S':
 
 
 #%%
-user_input = np.array([[pclass_input, pclass_input, sib_input, prach_input, df.loc[0, 'Cabin_No'], gender, embarked]])
+user_input = np.array([[pclass_input, Gender_input,age_input, sib_input, prach_input, fare_input, embarked]])
 print(user_input)
 
 #%%
